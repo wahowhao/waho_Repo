95f691fd5dcbe97412e215e68b20f08a261dedda
wahowaho <m-tanno@hykwlab.org>
2019-06-01 20:30:03 +0900

    Javaの練習用に作成

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int x;
+
+        x = 3;
+
+        System.out.println(x);
+
+        x = 3 + x;
+
+        System.out.println(x);
+
+    }
+}
